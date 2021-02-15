9d47e0659ed2d730e4012d92588af50481929dfb
PhilipB20 <70744825+PhilipB20@users.noreply.github.com>
2021-02-15 09:39:27 +0100

    Add files via upload

---
+++ Prov/Prov/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string HP = "[=====]";
+            int HP1 = 5;
+            while (HP1 > 0)
+            {
+                Console.WriteLine(HP);
+                Console.WriteLine("Hur mycket skada från 1-2 vill du förlora?");
+                string Fight1 = "";
+                Console.ReadLine();
+                if (Fight1 != "1")
+                {
+                    HP1 = -1;
+                    Console.WriteLine("Du förlorade 1 HP");
+                }
+                else if (Fight1 != "2")
+                {
+                    HP1 = -2;
+                    Console.WriteLine("Du förlorade 2 HP");
+                }
+                else
+                {
+                    Console.WriteLine("Försök igen!");
+                }
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
